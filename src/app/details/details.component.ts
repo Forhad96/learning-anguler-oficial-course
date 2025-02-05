ea00915b7f4961f27a4eaeb5c11421c645bc9df7
FORHAD <forhadairdrop@gmail.com>
2025-02-06 05:51:45 +0600

    refactor details component to use HousingService for data retrieval and display housing location details

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,20 +1,21 @@
-import { Component ,inject} from '@angular/core';
-import { ActivatedRoute } from '@angular/router'; 
+import { Component, inject } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { HousingService } from "../housing.service";
+import { HousingLocation } from "../housinglocation";
 
 @Component({
-  selector: 'app-details',
+  selector: "app-details",
   imports: [],
-  template: `
-    <p>
-      details works! {{houseLocationId}}
-    </p>
-  `,
-  styles: ``
+  template: ` <p>details works! {{ housingLocation?.id }},{{ housingLocation?.name }}</p> `,
+  styles: ``,
 })
 export class DetailsComponent {
-routeParams:ActivatedRoute = inject(ActivatedRoute)
-houseLocationId = 0;
-constructor(){
-  this.houseLocationId = Number(this.routeParams.snapshot.params['id']);
-}
+  routeParams: ActivatedRoute = inject(ActivatedRoute);
+  housingService: HousingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
+  constructor() {
+    const houseLocationId = Number(this.routeParams.snapshot.params["id"]);
+    this.housingLocation =
+      this.housingService.getHousingLocationById(houseLocationId);
+  }
 }
