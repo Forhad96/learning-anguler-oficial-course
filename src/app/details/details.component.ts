5ba034c5add128f20f2d1d8800e28a7b0c5f1370
FORHAD <forhadairdrop@gmail.com>
2025-02-06 07:55:37 +0600

    enhance details component with improved layout and styling for housing location display

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -6,8 +6,34 @@ import { HousingLocation } from "../housinglocation";
 @Component({
   selector: "app-details",
   imports: [],
-  template: ` <p>details works! {{ housingLocation?.id }},{{ housingLocation?.name }}</p> `,
-  styles: ``,
+  template: `
+    <article>
+      <img
+        class="listing-photo"
+        [src]="housingLocation?.photo"
+        alt="Exterior photo of {{ housingLocation?.name }}"
+      />
+      <div class="listing-heading">{{ housingLocation?.name }}</div>
+      <p class="listing-location">
+        ,{{ housingLocation?.city }},{{ housingLocation?.state }}
+      </p>
+      <section class="listing-features">
+        <h2 class="section-heading">About this house location</h2>
+        <ul>
+          <li>Units available:{{housingLocation?.availableUnits}}
+          </li>
+          <li>Dose this location have wifi{{housingLocation?.wifi}}</li>
+          <li>Dose this location have laundry{{housingLocation?.laundry}}</li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+
+      <h2 class="section-heading">Apply now to live here</h2>
+      <button class="primary">Apply Now</button>
+      </section>
+    </article>
+  `,
+  styleUrl: `./details.component.css`, 
 })
 export class DetailsComponent {
   routeParams: ActivatedRoute = inject(ActivatedRoute);
